100933ac91a2e7e4230062f67b6549ddbff61d04
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-05-23 10:54:07 +0600

    create logger middleware

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ import express,
 import { issueRoute } from './modules/issue/issue.route';
 import { authRoute } from './modules/auth/auth.route';
 
+import fs from 'fs';
+
 
 const app: Application = express()
 
@@ -15,6 +17,37 @@ const app: Application = express()
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+
+
+
+
+app.use((req, res, next) => {
+
+    console.log("Method -- Time --url:", req.method, Date.now(), req.url);
+
+    
+    const log = `${req.method} -- ${Date.now()} -- ${req.url}\n`;
+
+    fs.appendFile('server.log', log, (err) => {
+        if (err) {
+            console.log("Error writing to log file:", err);
+        }
+    });
+
+    
+    next();
+
+}
+
+);
+
+
+
+
+
+
+
+
 
 app.get('/', (req: Request, res: Response) => {
 
